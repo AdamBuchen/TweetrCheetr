2570ffea4060810b6c439f886cef1c5dfc189ad2
Adam Buchen <adam.buchen@yahoo.com>
2014-01-13 15:13:10 -0500

    Got config importing working

--- tweetrcheetr.py
+++ tweetrcheetr.py
@@ -1,9 +1,18 @@
+import sys
+from config import config
 from flask import Flask
+from flask import request
+from pprint import pprint
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Helloo"
+	print config.get('TWITTER','TWITTER_API_KEY')
+	#return request.args.get('a');
+	#return "Helloo"
 
-if __name__ == "__main__":
-	app.run(host='www.tweetrcheetr.com', port=8080)
+hello()
+
+#if __name__ == "__main__":
+#	app.run(host='www.tweetrcheetr.com', port=8080)
