47a1254322aa9db67a3495cc4dbaaaea7482f35c
Adam Buchen <adam.buchen@yahoo.com>
2014-07-26 02:19:01 -0400

    Got it outputting lots of crazy stuff

--- parser/parser.py
+++ parser/parser.py
@@ -15,6 +15,17 @@ rownum = 0;
 mentions = {}
 hashtags = {}
 words = {}
+retweeters = {}
+favoriters = {}
+repliers = {}
+interactors = {}
+top_favorited_tweets = {}
+top_retweeted_tweets = {}
+top_replied_tweets = {}
+top_interacted_tweets = {}
+total_num_retweets = 0
+total_num_favorites = 0
+total_num_replies = 0
 
 ##regexes
 mention_regex = "\@[A-Za-z0-9_]+"
@@ -54,14 +65,14 @@ def get_replies(html):
 
 	return replies
 
-def get_remote_tweet(user_name, tweet_id, system_path, tweet_data):
+def get_remote_tweet(user_name, tweet_id, system_path, tweet_data={}):
 	file_name = system_path + "/" + str(user_name) + "_" + str(tweet_id) + ".json"
 	try:
 		f = open(file_name, 'r')
-		print "Loading from file Cache"
+		#print "Loading from file Cache"
 		json_text = f.read()
 	except:
-		print "Fetching from Twitter"
+		#print "Fetching from Twitter"
 		retweet_regex = re.compile("Retweets <strong>(.*?)</strong>")
 		favorite_regex = re.compile("Favorites <strong>(\d+)<\/strong>")
 		user_regex = r'data-user-id="(\d+)"(.*?)data-screen-name="(.*?)"(.*?)data-name="(.*?)"(.*?)profile-avatar(.*?)src="(.*?)"(.*?)strong(\d+)\/strong Followers'
@@ -164,10 +175,14 @@ i = 0
 
 for single_tweet in reversed(tweets):
 	i += 1
-	print str(i) + " of " + str(total_num_tweets) + " - " + str(single_tweet["tweet_id"])	
+	#print str(i) + " of " + str(total_num_tweets) + " - " + str(single_tweet["tweet_id"])	
 	single_tweet = get_remote_tweet(twitter_username,single_tweet["tweet_id"],output_path,single_tweet)
-	print single_tweet
+	#print single_tweet
 	tweet_text = single_tweet["text"].lower()
+	substr = tweet_text[0:4]
+	if substr == "rt @":
+		continue
+	
 	tweet_text_raw = tweet_text
 	tweet_mentions = re.findall(mention_regex,tweet_text)
 	for single_mention in tweet_mentions:
@@ -199,28 +214,182 @@ for single_tweet in reversed(tweets):
 	except:
 		pass 
 
+	
+	num_retweets = int(single_tweet["num_retweets"])
+	num_favorites = int(single_tweet["num_favorites"])
+	num_replies = int(single_tweet["num_replies"])
+	
+	#TODO Possibly assign weighting so retweets are worth more than favorites, etc.
+	num_interactions = num_retweets + num_favorites + num_replies
+
+	total_num_retweets += num_retweets
+	total_num_favorites += num_favorites
+	total_num_replies += num_replies
+
+	top_retweeted_tweets[single_tweet["tweet_id"]] = num_retweets
+	top_favorited_tweets[single_tweet["tweet_id"]] = num_favorites
+	top_replied_tweets[single_tweet["tweet_id"]] = num_replies
+	top_interacted_tweets[single_tweet["tweet_id"]] = num_interactions
+	
+	for user in single_tweet["retweeters"]:
+		user_name = str(user["user_name"])
+		if user_name in retweeters:
+			retweeters[user_name] += 1
+		else:
+			retweeters[user_name] = 1
+		if user_name in interactors:
+			interactors[user_name] += 1
+		else:
+			interactors[user_name] = 1
+
+	
+	for user in single_tweet["favoriters"]:
+		user_name = str(user["user_name"])
+		if user_name in favoriters:
+			favoriters[user_name] += 1
+		else:
+			favoriters[user_name] = 1
+		if user_name in interactors:
+			interactors[user_name] += 1
+		else:
+			interactors[user_name] = 1
+
+	for reply in single_tweet["replies"]:
+		user_name = str(reply["user_name"])
+		if user_name == "@" + twitter_username.lower():
+			continue
+
+		if user_name in repliers:
+			repliers[user_name] += 1
+		else:
+			repliers[user_name] = 1
+		if user_name in interactors:
+			interactors[user_name] += 1
+		else:
+			interactors[user_name] = 1
+
+
+print "### TOP MENTIONS ###"
 sorted_mentions = sorted(mentions.items(), key=itemgetter(1), reverse=True)
 sorted_mentions = sorted_mentions[0:20]
 for idx, mention_user in enumerate(sorted_mentions):
 	rank = str(idx + 1)
 	print rank + ": " + mention_user[0] + " (" + str(mention_user[1]) + ")"
 
-print "\n"
+print "\n\n"
 
+print "### TOP HASHTAGS ###"
 sorted_hashtags = sorted(hashtags.items(), key=itemgetter(1), reverse=True)
 sorted_hashtags = sorted_hashtags[0:20]
 for idx, mention_hashtag in enumerate(sorted_hashtags):
 	rank = str(idx + 1)
 	print rank + ": " + mention_hashtag[0] + " (" + str(mention_hashtag[1]) + ")"
 
-print "\n"
+print "\n\n"
 
+print "### TOP WORDS ###"
 sorted_words = sorted(words.items(), key=itemgetter(1), reverse=True)
 sorted_words = sorted_words[0:10]
 for idx, mention_word in enumerate(sorted_words):
 	rank = str(idx + 1)
 	print rank + ": " + str(mention_word[0]) + " (" + str(mention_word[1]) + ")"
 
+print "\n\n"
+
+print "### TOP RETWEETERS ###"
+sorted_retweeters = sorted(retweeters.items(), key=itemgetter(1), reverse=True)
+sorted_retweeters = sorted_retweeters[0:50]
+for idx, user_name in enumerate(sorted_retweeters):
+	rank = str(idx + 1)
+	print rank + ": " + str(user_name[0]) + " (" + str(user_name[1]) + ")"
+
+print "\n\n"
+print "### TOP FAVORITERS ###"
+sorted_favoriters = sorted(favoriters.items(), key=itemgetter(1), reverse=True)
+sorted_favoriters = sorted_favoriters[0:50]
+for idx, user_name in enumerate(sorted_favoriters):
+	rank = str(idx + 1)
+	print rank + ": " + str(user_name[0]) + " (" + str(user_name[1]) + ")"
+
+
+print "\n\n"
+print "### TOP REPLIERS ###"
+sorted_repliers = sorted(repliers.items(), key=itemgetter(1), reverse=True)
+sorted_repliers = sorted_repliers[0:50]
+for idx, user_name in enumerate(sorted_repliers):
+	rank = str(idx + 1)
+	print rank + ": " + str(user_name[0]) + " (" + str(user_name[1]) + ")"
+
+
+print "\n\n"
+print "### TOP INTERACTORS ###"
+sorted_interactors = sorted(interactors.items(), key=itemgetter(1), reverse=True)
+sorted_interactors = sorted_interactors[0:50]
+for idx, user_name in enumerate(sorted_interactors):
+	rank = str(idx + 1)
+	print rank + ": " + str(user_name[0]) + " (" + str(user_name[1]) + ")"
+
+
+print "\n\n"
+print "### TOP RETWEETED TWEETS ###"
+sorted_top_retweeted = sorted(top_retweeted_tweets.items(), key=itemgetter(1), reverse=True)
+sorted_top_retweeted = sorted_top_retweeted[0:25]
+for idx, tweet_info in enumerate(sorted_top_retweeted):
+	rank = str(idx + 1)
+	tweet_id = tweet_info[0]
+	num_retweets = int(tweet_info[1])
+	single_tweet = get_remote_tweet(twitter_username,tweet_id,output_path,{})
+	tweet_text = str(single_tweet["text"])
+	tweet_timestamp = str(single_tweet["timestamp"])
+	print rank + ": " + tweet_text + " (" + tweet_timestamp + ") (" + str(num_retweets) + ")"
+
+
+print "\n\n"
+print "### TOP FAVORITED TWEETS ###"
+sorted_top_favorited = sorted(top_favorited_tweets.items(), key=itemgetter(1), reverse=True)
+sorted_top_favorited = sorted_top_favorited[0:25]
+for idx, tweet_info in enumerate(sorted_top_favorited):
+	rank = str(idx + 1)
+	tweet_id = tweet_info[0]
+	num_retweets = int(tweet_info[1])
+	single_tweet = get_remote_tweet(twitter_username,tweet_id,output_path,{})
+	tweet_text = str(single_tweet["text"])
+	tweet_timestamp = str(single_tweet["timestamp"])
+	print rank + ": " + tweet_text + " (" + tweet_timestamp + ") (" + str(num_retweets) + ")"
+
+
+print "\n\n"
+print "### TOP REPLIED TWEETS ###"
+sorted_top_replied = sorted(top_replied_tweets.items(), key=itemgetter(1), reverse=True)
+sorted_top_replied = sorted_top_replied[0:25]
+for idx, tweet_info in enumerate(sorted_top_replied):
+	rank = str(idx + 1)
+	tweet_id = tweet_info[0]
+	num_retweets = int(tweet_info[1])
+	single_tweet = get_remote_tweet(twitter_username,tweet_id,output_path,{})
+	tweet_text = str(single_tweet["text"])
+	tweet_timestamp = str(single_tweet["timestamp"])
+	print rank + ": " + tweet_text + " (" + tweet_timestamp + ") (" + str(num_retweets) + ")"
+
+
+print "\n\n"
+print "### TOP INTERACTED TWEETS ###"
+sorted_top_interacted = sorted(top_interacted_tweets.items(), key=itemgetter(1), reverse=True)
+sorted_top_interacted = sorted_top_interacted[0:25]
+for idx, tweet_info in enumerate(sorted_top_interacted):
+	rank = str(idx + 1)
+	tweet_id = tweet_info[0]
+	num_retweets = int(tweet_info[1])
+	single_tweet = get_remote_tweet(twitter_username,tweet_id,output_path,{})
+	tweet_text = str(single_tweet["text"])
+	tweet_timestamp = str(single_tweet["timestamp"])
+	print rank + ": " + tweet_text + " (" + tweet_timestamp + ") (" + str(num_retweets) + ")"
+
+print "\n\n"
+print "#### TOTAL RETWEETS: " + str(total_num_retweets) + " ####"
 print "\n"
-
-
+print "#### TOTAL FAVORITES: " + str(total_num_favorites) + " ####"
+print "\n"
+print "#### TOTAL REPLIES: " + str(total_num_replies) + " ####"
+print "\n"
+print "\n"
